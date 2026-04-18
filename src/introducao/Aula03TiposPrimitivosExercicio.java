5ad1463a1e284e18380010b395a36c036f637100
Galdino <jpgaldino.dev@gmail.com>
2026-04-18 16:38:50 -0300

    Aula03: exercício tipos Primitivos e variáveis

---
+++ src/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,12 @@
+package introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String nome = "João";
+        String endereco = "Umbuzeiro";
+        double salario = 4.000;
+        String data = "12/10/2004";
+
+        System.out.println("Eu, " + nome + ", morando no endereço: " + endereco + ", confirmo que recebi o salário de " + salario + " na data " + data);
+    }
+}
