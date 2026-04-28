32577dfe548db48806cb44c16833b4e9b74d78d1
Galdino <jpgaldino.dev@gmail.com>
2026-04-28 11:28:26 -0300

    Aula05: Estruturas condicionais 6

---
+++ src/introducao/Aula05EstruturasCondicionais06.java
@@ -0,0 +1,18 @@
+package introducao;
+
+public class Aula05EstruturasCondicionais06 {
+    public static void main(String[] args) {
+        byte dia = 2;
+
+        switch (dia) {
+            case 1, 7:
+                System.out.println("Final de semana");
+                break;
+            case 2, 3, 4, 5, 6:
+                System.out.println("Dia útil");
+                break;
+            default:
+                System.out.println("Número inválido");
+        }
+    }
+}
