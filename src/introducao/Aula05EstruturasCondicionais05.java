0adb4350bd82f9b283206695c8b5199c424d9c80
Galdino <jpgaldino.dev@gmail.com>
2026-04-28 10:30:37 -0300

    Aula05: Estruturas condicionais 5

---
+++ src/introducao/Aula05EstruturasCondicionais05.java
@@ -0,0 +1,46 @@
+package introducao;
+
+public class Aula05EstruturasCondicionais05 {
+    public static void main(String[] args) {
+        int dia = 1;
+        switch (dia) {
+            case 1:
+                System.out.println("dom");
+                break;
+            case 2:
+                System.out.println("Seg");
+                break;
+            case 3:
+                System.out.println("ter");
+                break;
+            case 4:
+                System.out.println("qua");
+                break;
+            case 5:
+                System.out.println("qui");
+                break;
+            case 6:
+                System.out.println("Sex");
+                break;
+            case 7:
+                System.out.println("Sab");
+                break;
+            default:
+                System.out.println("Opção inválida");
+                break;
+        }
+
+        char sexo = 'M';
+
+        switch (sexo) {
+            case 'M':
+                System.out.println("Homem");
+                break;
+            case 'F':
+                System.out.println("Mulher");
+                break;
+            default:
+                System.out.println("Inválido");
+        }
+    }
+}
