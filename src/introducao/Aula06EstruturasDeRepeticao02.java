b390be9932f719c0675c2d629b46d6a77df9a6aa
Galdino <jpgaldino.dev@gmail.com>
2026-04-29 10:11:09 -0300

    Aula06: Estruturas de repetição 2

---
+++ src/introducao/Aula06EstruturasDeRepeticao02.java
@@ -0,0 +1,12 @@
+package introducao;
+
+public class Aula06EstruturasDeRepeticao02 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println("No. " + i);
+            }
+        }
+    }
+}
