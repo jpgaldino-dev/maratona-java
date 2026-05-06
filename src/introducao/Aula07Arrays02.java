03195dc3b7c55ac59b84f44c30e88cb935c53443
Galdino <jpgaldino.dev@gmail.com>
2026-05-06 10:24:08 -0300

    Aula07 : Arrays 2

---
+++ src/introducao/Aula07Arrays02.java
@@ -0,0 +1,20 @@
+package introducao;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+        // byte, short, int, long, float e double  0
+        // char '\u0000'
+        // boolean false
+        // String null
+
+        String[] nomes = new String[4];
+        nomes[0] = "Chico pantera";
+        nomes[1] = "Bufalo Bill";
+        nomes[2] = "Zeca urubu";
+        nomes[3] = "Goku";
+
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
+    }
+}
