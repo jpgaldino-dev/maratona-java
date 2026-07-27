3094f430277c1f5513fca98328de94e18bc66e75
Galdino <jpgaldino.dev@gmail.com>
2026-07-27 16:24:24 -0300

    Aula 05: Estruturas condicionais 3

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
@@ -4,7 +4,7 @@ public class Aula05EstruturasCondicionais03 {
     public static void main(String[] args) {
         double salario = 3000;
         String mensagemDoar = "Eu vou doar 500 para o devdojo";
-        String mensagemNaoDoar = "Ainda não tenho condições, mas vou ter!";
+        String mensagemNaoDoar = "Ainda não tenho condições, mas vou ter";
         String resultado = (salario > 5000) ? mensagemDoar : mensagemNaoDoar;
 
         System.out.println(resultado);
