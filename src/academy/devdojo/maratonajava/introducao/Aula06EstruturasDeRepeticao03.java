c209ae62f255e51559e6758c822f6909672aa115
Galdino <jpgaldino.dev@gmail.com>
2026-07-27 16:29:19 -0300

    Aula 06: Estruturas de repetição 3

--- src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao03.java
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao03.java
@@ -2,7 +2,7 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula06EstruturasDeRepeticao03 {
     public static void main(String[] args) {
-        int valorMax = 50;
+        int valorMax = 51;
 
         for (int i = 0; i <= valorMax; i++) {
             if (i > 25) {
