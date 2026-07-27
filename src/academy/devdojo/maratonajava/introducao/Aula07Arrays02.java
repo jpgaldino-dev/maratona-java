89634f4c1f04a3750494b991f717141cf08f7015
Galdino <jpgaldino.dev@gmail.com>
2026-07-27 16:30:37 -0300

    Aula 07: Arrays 2

--- src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -2,7 +2,7 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula07Arrays02 {
     public static void main(String[] args) {
-        // byte, short, int, long, float e double  0
+        // byte, short, int, long, float e double
         // char '\u0000'
         // boolean false
         // String null
