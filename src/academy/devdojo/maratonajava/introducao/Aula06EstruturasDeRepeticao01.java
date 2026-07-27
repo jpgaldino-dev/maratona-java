9b79cf39f5c4301292ab98f8768af2e830b5ec5d
Galdino <jpgaldino.dev@gmail.com>
2026-07-27 16:28:09 -0300

    Aula 06: Estruturas de repetição

--- src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao01.java
@@ -2,7 +2,7 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula06EstruturasDeRepeticao01 {
     public static void main(String[] args) {
-        int count = 0;
+        int count = 1;
 
         while (count < 10) {
             System.out.println(count);
