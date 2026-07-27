83a4aae1327db21aef47baa797fdd5c39cb1f5eb
Galdino <jpgaldino.dev@gmail.com>
2026-07-27 16:06:35 -0300

    Aula01 : Ola Mundo

--- src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -2,6 +2,6 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula01OlaDevDojo {
     public static void main(String[] args) {
-        System.out.println("Olá mundo!");
+        System.out.println("Olá Mundo!");
     }
 }
